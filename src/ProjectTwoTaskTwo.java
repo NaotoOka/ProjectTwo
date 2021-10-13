aea24a61d6f6fd9b3a867baa1cba613e3e6e5399
Naoto Oka <76252032+NaotoOka@users.noreply.github.com>
2021-10-12 19:47:03 -0700

    Add files via upload

---
+++ src/ProjectTwoTaskTwo.java
@@ -0,0 +1,28 @@
+//Naoto&Nicholas
+//10/10/21
+//ProjectTwo
+import java.util.Scanner;
+public class ProjectTwoTaskTwo {
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = input.next();
+		
+		double cost = 44.55;
+		String owners = "Naoto";
+		
+		if(owners.equalsIgnoreCase(name))
+		{
+			System.out.println("You are eligible for a $2.00 discount");
+			cost = cost - 2 ;
+			System.out.println("the cost " +cost);
+		}
+
+		
+
+	}
+
+}
